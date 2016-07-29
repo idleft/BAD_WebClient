864b06e2a849bdf21b52aabd299a3d8f663d5c11
Syed Shahbaaz Safir <safirshahbaaz90@gmail.com>
2016-07-29 15:59:37 -0700

    Multiple Subscriptions

--- static/js/MainController.js
+++ static/js/MainController.js
@@ -1,71 +1,81 @@
 app.controller('MainController', ['$scope', '$interval', '$websocket', '$window', '$location', 'HttpGetter', 
-	'SessionStorage', function($scope, $interval, $websocket, $window, $location, HttpGetter, SessionStorage) {
+    'SessionStorage', function($scope, $interval, $websocket, $window, $location, HttpGetter, SessionStorage) {
 
-	$scope.messages = [];
-	$scope.msgChannelName = '';
+    $scope.messages = [];
+    $scope.msgChannelName = '';
 
-	var successFunction = function(data) {
-		console.log("SAFIR-->All is well with new results!");
+    var successFunction = function(data) {
+        console.log("SAFIR-->All is well with new results!");
 
-		console.log(data);
+        console.log(data);
 
-		$scope.messages.reverse();
+        $scope.messages.reverse();
 
-		if(data['data']['results']!=null){
+        if(data['data']['results']!=null){
 
-		for(i = 0; i < data['data']['results'].length; i++) {
-			var message = {
-				'message' : data['data']['results'][i]['message'],
-				'timestamp' : data['data']['results'][i]['timestamp'],
-				'msgChannelName' : data['data']['channelName']
-			}
-			$scope.messages.push(message);
-		}
-	}
-		$scope.messages.reverse();
-	}
+            for(i = 0; i < data['data']['results'].length; i++) {
+                var message = {
+                    'message' : data['data']['results'][i]['message'],
+                    'timestamp' : data['data']['results'][i]['timestamp'],
+                    'msgChannelName' : data['data']['channelName']
+                }
+                $scope.messages.push(message);
+            }
+        }
+        $scope.messages.reverse();
+    }
 
-	var errorFunction = function(data) {
-		console.log("Something went wrong: " + data);
-	}
+    var errorFunction = function(data) {
+        console.log("Something went wrong: " + data);
+    }
 
-	$scope.parseMessage = function(message) {
-		console.log('Received websocket message from the server');
-		var data = JSON.parse(message.data);
-		console.log(data);
-		console.log($scope.userId);
-		console.log(data['userId']);
+    $scope.parseMessage = function(message) {
+        console.log('Received websocket message from the server');
+        var data = JSON.parse(message.data);
+        console.log(data);
+        console.log($scope.userId);
+        console.log(data['userId']);
 
-		if($scope.userId == data['userId']) {
-			$scope.latestTimeStamp = data['timestamp'];
-			console.log("timestamp:"+$scope.latestTimeStamp);
-			SessionStorage.set('timestamp', $scope.latestTimeStamp);
-			HttpGetter.getNewResults($scope.userId, $scope.accessToken, $scope.subscriptionId, 
-				$scope.latestTimeStamp, data['channelName'], successFunction, errorFunction);
-		}
-	}
+        if($scope.userId == data['userId']) {
+            $scope.latestTimeStamp = data['timestamp'];
 
-	$scope.logoutUser = function() {
-		$scope.dataStream.close();
-		$window.location.href = '/';
-	}
+            console.log("timestamp:"+$scope.latestTimeStamp);
+            //SessionStorage.set('timestamp', $scope.latestTimeStamp);
 
-	$scope.switchToSubscriptionsPage = function() {
-		$scope.dataStream.close();
-		$window.location.href = '/preferences';	
-	}
+            var subscriptionList = JSON.parse(SessionStorage.get('subscriptionId'));
 
-	$scope.init = function() {
-		$scope.userId = SessionStorage.get('userId');
-		$scope.accessToken = SessionStorage.get('accessToken');
-		$scope.subscriptionId = SessionStorage.get('subscriptionId');
-		$scope.latestTimeStamp = SessionStorage.get('timestamp');
+            function findSubcription(subscriptionId) {
+                return subscriptionId == data['userSubscriptionId'];
+            }
 
-		console.log('SAFIR-->Creating Web Socket');
-		socketAddress = "ws://" + $location.host() + ":8989/websocketlistener";
-                console.log(socketAddress);
-                $scope.dataStream = $websocket(socketAddress);
-		$scope.dataStream.onMessage($scope.parseMessage);
-	}
+            if(undefined != subscriptionList.find(findSubcription)) {
+                HttpGetter.getNewResults($scope.userId, $scope.accessToken, data['userSubscriptionId'], 
+                    $scope.latestTimeStamp, data['channelName'], successFunction, errorFunction);   
+            }
+        }
+    }
+
+    $scope.logoutUser = function() {
+        $scope.dataStream.close();
+        $window.location.href = '/';
+    }
+
+    $scope.switchToSubscriptionsPage = function() {
+        $scope.dataStream.close();
+        $window.location.href = '/preferences'; 
+    }
+
+    $scope.init = function() {
+        $scope.userId = SessionStorage.get('userId');
+        $scope.accessToken = SessionStorage.get('accessToken');
+        $scope.subscriptionId = SessionStorage.get('subscriptionId');
+        $scope.latestTimeStamp = SessionStorage.get('timestamp');
+
+        console.log('SAFIR-->Creating Web Socket');
+        socketAddress = "ws://" + $location.host() + ":8989/websocketlistener";
+        console.log(socketAddress);
+        $scope.dataStream = $websocket(socketAddress);
+        $scope.dataStream.onMessage($scope.parseMessage);
+    }
 
 }]);
--- static/js/SessionStorage.js
+++ static/js/SessionStorage.js
@@ -1,8 +1,25 @@
 app.factory('SessionStorage', function($window) {
 	function set(key, data) {
-		$window.sessionStorage.setItem(key, data)
+
+		if(key == 'subscriptionId' || key == 'timestamp') {
+			if($window.sessionStorage.getItem(key)) {
+				var keyList = JSON.parse($window.sessionStorage.getItem(key));
+				console.log(keyList);
+				keyList.push(data);
+				$window.sessionStorage.setItem(key, JSON.stringify(keyList));
+			}
+			else {
+				var keyList = [data];
+				$window.sessionStorage.setItem(key, JSON.stringify(keyList));
+			}
+		}
+		else {
+			$window.sessionStorage.setItem(key, data)	
+		}
+		
 		console.log($window.sessionStorage.getItem(key));
 	}
+
 	function get(key) {
 		return $window.sessionStorage.getItem(key);
 	}
