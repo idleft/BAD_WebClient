55b6e4b20fb985d686d405fc88146414c0fdbce5
purvikaul <kaulpurvi92@gmail.com>
2016-08-01 14:06:35 -0700

    Impact Zone edits

--- static/js/MainController.js
+++ static/js/MainController.js
@@ -1,17 +1,17 @@
-app.controller('MainController', ['$scope', '$interval', '$websocket', '$window', '$location', 'HttpGetter',
-    'SessionStorage', function ($scope, $interval, $websocket, $window, $location, HttpGetter, SessionStorage) {
+app.controller('MainController', ['$scope', '$interval', '$websocket', '$window', '$location', 'HttpGetter', 
+    'SessionStorage', function($scope, $interval, $websocket, $window, $location, HttpGetter, SessionStorage) {
 
-        $scope.messages = [];
-        $scope.msgChannelName = '';
+    $scope.messages = [];
+    $scope.msgChannelName = '';
 
-        var successFunction = function (data) {
-            console.log("SAFIR-->All is well with new results!");
+    var successFunction = function(data) {
+        console.log("SAFIR-->All is well with new results!");
 
-            console.log(data);
+        console.log(data);
 
-            $scope.messages.reverse();
+        $scope.messages.reverse();
 
-            if (data['data']['results'] != null) {
+        if(data['data']['results']!=null){
 
                 for (i = 0; i < data['data']['results'].length; i++) {
                     var d = data['data']['results'][i]['impactZone'].toString();
@@ -29,50 +29,60 @@ app.controller('MainController', ['$scope', '$interval', '$websocket', '$window'
                     $scope.messages.push(message);
                 }
             }
-            $scope.messages.reverse();
         }
+        $scope.messages.reverse();
+    }
 
-        var errorFunction = function (data) {
-            console.log("Something went wrong: " + data);
-        }
+    var errorFunction = function(data) {
+        console.log("Something went wrong: " + data);
+    }
 
-        $scope.parseMessage = function (message) {
-            console.log('Received websocket message from the server');
-            var data = JSON.parse(message.data);
-            console.log(data);
-            console.log($scope.userId);
-            console.log(data['userId']);
+    $scope.parseMessage = function(message) {
+        console.log('Received websocket message from the server');
+        var data = JSON.parse(message.data);
+        console.log(data);
+        console.log($scope.userId);
+        console.log(data['userId']);
 
-            if ($scope.userId == data['userId']) {
-                $scope.latestTimeStamp = data['timestamp'];
-                console.log("timestamp:" + $scope.latestTimeStamp);
-                SessionStorage.set('timestamp', $scope.latestTimeStamp);
-                HttpGetter.getNewResults($scope.userId, $scope.accessToken, $scope.subscriptionId,
-                    $scope.latestTimeStamp, data['channelName'], successFunction, errorFunction);
+        if($scope.userId == data['userId']) {
+            $scope.latestTimeStamp = data['timestamp'];
+
+            console.log("timestamp:"+$scope.latestTimeStamp);
+            //SessionStorage.set('timestamp', $scope.latestTimeStamp);
+
+            var subscriptionList = JSON.parse(SessionStorage.get('subscriptionId'));
+
+            function findSubcription(subscriptionId) {
+                return subscriptionId == data['userSubscriptionId'];
+            }
+
+            if(undefined != subscriptionList.find(findSubcription)) {
+                HttpGetter.getNewResults($scope.userId, $scope.accessToken, data['userSubscriptionId'], 
+                    $scope.latestTimeStamp, data['channelName'], successFunction, errorFunction);   
             }
         }
+    }
 
-        $scope.logoutUser = function () {
-            $scope.dataStream.close();
-            $window.location.href = '/';
-        }
+    $scope.logoutUser = function() {
+        $scope.dataStream.close();
+        $window.location.href = '/';
+    }
 
-        $scope.switchToSubscriptionsPage = function () {
-            $scope.dataStream.close();
-            $window.location.href = '/preferences';
-        }
+    $scope.switchToSubscriptionsPage = function() {
+        $scope.dataStream.close();
+        $window.location.href = '/preferences'; 
+    }
 
-        $scope.init = function () {
-            $scope.userId = SessionStorage.get('userId');
-            $scope.accessToken = SessionStorage.get('accessToken');
-            $scope.subscriptionId = SessionStorage.get('subscriptionId');
-            $scope.latestTimeStamp = SessionStorage.get('timestamp');
+    $scope.init = function() {
+        $scope.userId = SessionStorage.get('userId');
+        $scope.accessToken = SessionStorage.get('accessToken');
+        $scope.subscriptionId = SessionStorage.get('subscriptionId');
+        $scope.latestTimeStamp = SessionStorage.get('timestamp');
 
-            console.log('SAFIR-->Creating Web Socket');
-            socketAddress = "ws://" + $location.host() + ":8989/websocketlistener";
-            console.log(socketAddress);
-            $scope.dataStream = $websocket(socketAddress);
-            $scope.dataStream.onMessage($scope.parseMessage);
-        }
-
-    }]);
+        console.log('SAFIR-->Creating Web Socket');
+        socketAddress = "ws://" + $location.host() + ":8989/websocketlistener";
+        console.log(socketAddress);
+        $scope.dataStream = $websocket(socketAddress);
+        $scope.dataStream.onMessage($scope.parseMessage);
+    }
+}]);
