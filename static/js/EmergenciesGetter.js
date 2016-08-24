ad68af80859d028bc0880ac8d9d645ee585899c0
purvikaul <kaulpurvi92@gmail.com>
2016-08-24 13:55:54 -0700

    Location Tab changes

---
+++ static/js/EmergenciesGetter.js
@@ -0,0 +1,12 @@
+app.factory('EmergencyGetter',function(){
+var emergency=[
+            {label: "Earthquake", val: false},
+            {label: "Hurricane", val: false},
+            {label: "Tornado", val: false},
+            {label: "Flood", val: false},
+            {label: "Shooting", val: false}
+        ];
+return emergency;
+}
+	
+});
--- static/js/SubscriptionCtrl.js
+++ static/js/SubscriptionCtrl.js
@@ -1,18 +1,12 @@
-app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStorage', 'SubscriptionGetter', 'geolocationService',
-    function ($scope,$window, $filter, SessionStorage, SubscriptionGetter, geolocationService) {
+app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStorage', 'SubscriptionGetter', 'geolocationService','EmergencyGetter'
+    , function ($scope,$window, $filter, SessionStorage, SubscriptionGetter, geolocationService,EmergencyGetter) {
 
         console.log("In SubscriptionCtrl");
 
         $scope.accessToken = SessionStorage.get('accessToken');
         $scope.userId = SessionStorage.get('userId');
 
-        $scope.chkbxs = [
-            {label: "Earthquake", val: false},
-            {label: "Hurricane", val: false},
-            {label: "Tornado", val: false},
-            {label: "Flood", val: false},
-            {label: "Shooting", val: false}
-        ];
+        $scope.chkbxs = EmergencyGetter;
 
         $scope.params = [
             "Earthquake", "Hurricane", "Tornado", "Flood", "Shooting"
@@ -28,15 +22,12 @@ app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStora
 
         var emergencySuccessFunction = function(data) {
             console.log("All is well with subscriptions!");
-
             SessionStorage.set('subscriptionId', data['data']['userSubscriptionId']);
             SessionStorage.set('timestamp', data['data']['timestamp']);
-
             counter++;
             console.log("counter:" + counter);
-
             if(counter == $scope.length){
-                $window.location.href = '/notifications';
+                $window.location.href = '/locationsubs';
             }
         };
 
@@ -65,11 +56,9 @@ app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStora
 
         $scope.onClickNearMe = function () {
             console.log("In onClickNearMe");
-
             if ($scope.nearMe) {
                 var d = geolocationService.getCurrentPosition().then(UserPosition);
                 var subscriptionList = getSubscriptionList();
-
                 d.then(function() {
                     for(i = 0; i < subscriptionList.length; i++){
                         console.log("Subscribing for:"+subscriptionList[i]);
@@ -83,26 +72,21 @@ app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStora
 
         $scope.subscribeToShelterInfo=function(){
             console.log("In subscribeToShelterInfo");
-
             var subscriptionList = getSubscriptionList();
             if($scope.shelterInfo) {
                 if (!$scope.nearMe) {
                     var d = geolocationService.getCurrentPosition().then(UserPosition);
                     d.then(function() {
-
                         for(i = 0; i < subscriptionList.length; i++) {
                             console.log("Subscribing for:"+subscriptionList[i]);
-
                             SubscriptionGetter.postEmergenciesLoctionWithSheltersSubscription($scope.userId,
                                 $scope.mylocation, $scope.accessToken, subscriptionList[i],
                                 successFunction, errorFunction)
                         }
-
                     });
                 } else {
                     for(i = 0; i < subscriptionList.length; i++){
                         console.log("Subscribing for:"+subscriptionList[i]);
-
                         SubscriptionGetter.postEmergenciesLoctionWithSheltersSubscription($scope.userId,
                             $scope.mylocation, $scope.accessToken, subscriptionList[i],
                             successFunction, errorFunction)
@@ -114,51 +98,36 @@ app.controller('SubscriptionCtrl', ['$scope', '$window','$filter', 'SessionStora
 
         function getSubscriptionList() {
             console.log("In getSubscriptionList");
-
             var subscriptionList = $filter('filter')($scope.chkbxs, {val: true});
-
             console.log("Just testing filter"+subscriptionList);
-
             for (var i = 0; i < subscriptionList.length; i++) {
                 subscriptionList[i] = angular.lowercase(subscriptionList[i].label);
-
                 console.log("the list "+subscriptionList[i]);
             }
-
             $scope.length = subscriptionList.length;
             console.log("length of subscriptionList:"+$scope.length);
             return subscriptionList;
-
         }
 
         $scope.subscribeToEmergencies = function () {
             $scope.isActive = true;
             var i;
-            //var subscriptionList = getSubscriptionList();
-
             console.log("SAFIR" + $scope.isChecked);
-
             if($scope.isChecked == true) {
                 var subscriptionList = $scope.params;
             }
             else {
                 var subscriptionList = getSubscriptionList();
             }
-
             console.log(subscriptionList);
-
             $scope.accessToken = SessionStorage.get('accessToken');
             $scope.userId = SessionStorage.get('userId');
-
             for(i = 0; i < subscriptionList.length; i++){
                 console.log("Subscribing for:" + subscriptionList[i]);
-
                 var successFunctionUsed = successFunction;
-
                 if(i == subscriptionList.length - 1) {
                     successFunctionUsed = emergencySuccessFunction
                 }
-
                 SubscriptionGetter.postEmergenciesSubscription($scope.userId, $scope.accessToken,
                     subscriptionList[i], emergencySuccessFunction, errorFunction);
             }
